348ac6e0ccb6ab80882d126e94088fba3bf11ec1
Chinmayee-Hugar27 <82188378+Chinmayee-Hugar27@users.noreply.github.com>
2021-06-10 18:45:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+menu_list_array = ["Veg Margherita Pizza",
+                   " Chicken Tandoori Pizza",
+                   "Veg Supreme Pizza",
+                   "Paneer Tikka Pizza",
+                   "Deluxe Veggie Pizza",
+                   "Veg Extravaganza Pizza"];
+
+function getmenu(){
+var htmldata;
+//Complete the code
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+//Complete the code
+
+}
+
+function add_top(){
+//Complete the code
+}
